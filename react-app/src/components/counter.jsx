c046bf81eab835484b368ea29c2869873d107a14
Sumaya Altamimi <smsomi9@hotmail.com>
2021-06-12 16:52:37 +0300

    some styles

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -3,20 +3,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { counter: 0 };
+  state = { counter: 0, imgUrl: "https://picsum.photos/200" };
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
   render() {
     // React.Fragment instead of div, no multiple div and root div in index.html
     return (
+      //braces {} to render valuesdynamically
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imgUrl} alt="" /> */}
+
+        {/* first way to apply styles by reference */}
+        <span style={this.styles} className="badge-primary">
+          {this.formatCount()}
+        </span>
+
+        {/* the second way is to apply inline styles */}
+        <button
+          style={{ fontSize: 20, fontWeight: "bold" }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
   formatCount() {
     const { counter } = this.state;
-    const hi = <h1>Hello frmo jsx </h1>; // we can do this in .jsx
-    return counter == 0 ? <h1>Zero</h1> : counter; // or "Zero"
+    // const hi = <h1>Hello frmo jsx </h1>; // we can do this in .jsx
+    return counter == 0 ? "Zero" : counter; // or inside h1
   }
 }
 
