6da778c687ad3927f766cefce335d4ddaf92891b
Sumaya Altamimi <smsomi9@hotmail.com>
2021-06-20 16:20:39 +0300

    learn

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -3,37 +3,63 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { counter: 0, imgUrl: "https://picsum.photos/200" };
+  state = {
+    counter: 0,
+    imgUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
   styles = {
     fontSize: 20,
     fontWeight: "bold",
   };
-  render() {
-    // React.Fragment instead of div, no multiple div and root div in index.html
-    return (
-      //braces {} to render valuesdynamically
-      <React.Fragment>
-        {/* <img src={this.state.imgUrl} alt="" /> */}
 
-        {/* first way to apply styles by reference */}
-        <span style={this.styles} className="badge-primary">
-          {this.formatCount()}
-        </span>
-
-        {/* the second way is to apply inline styles */}
-        <button
-          style={{ fontSize: 20, fontWeight: "bold" }}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-      </React.Fragment>
-    );
+  getClasses() {
+    let classes = "";
+    classes +=
+      this.state.counter === 0
+        ? "btn-secondary btn-sm btn-warning"
+        : "btn-secondary btn-sm";
+    return classes;
   }
+
   formatCount() {
     const { counter } = this.state;
     // const hi = <h1>Hello frmo jsx </h1>; // we can do this in .jsx
     return counter == 0 ? "Zero" : counter; // or inside h1
+  }
+
+  renderTags() {
+    if (this.state.tags.length == 0) return <p>Ther are no tags!</p>; // or plain text
+    return (
+      <ul>
+        {" "}
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  render() {
+    let classes = this.getClasses();
+
+    // React.Fragment instead of div, no multiple div and root div in index.html
+    return (
+      //braces {} to render valuesdynamically
+      <React.Fragment>
+        <img src={this.state.imgUrl} alt="" />
+        <br></br>
+        {/* first way to apply styles by reference */}
+        <span style={this.styles} className={this.getClasses()}>
+          {this.formatCount()}
+        </span>
+        <br></br>
+        {/* the second way is to apply inline styles */}
+        <button style={{ fontSize: 20, fontWeight: "bold" }}>Increment</button>
+        <br></br>
+        {this.renderTags()}
+      </React.Fragment>
+    );
   }
 }
 
